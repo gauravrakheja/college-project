b3d0deb659f498621de0e5b71a32ed23f515c952
gaurav <gauravrakheja002@gmail.com>
2017-07-16 15:59:43 +0530

    added users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170714154026) do
+ActiveRecord::Schema.define(version: 20170716102755) do
 
   create_table "books", force: :cascade do |t|
     t.string "name"
     t.string "author"
     t.string "publication"
     t.text "description"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "roll_number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
